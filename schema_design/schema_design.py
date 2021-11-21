c2c8d1df6d78a61f69ff8c7b3d46ede2c1933ba4
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2021-11-21 11:55:08 +0300

    выгрузка фильмов из таблицы movies в таблицу film_work

--- schema_design/schema_design.py
+++ schema_design/schema_design.py
@@ -43,12 +43,35 @@ def load_person(cursor):
     )
 
 
+def load_films(cursor):
+    films = [film for film in cursor.execute("""SELECT DISTINCT title, plot, imdb_rating FROM movies""")]
+
+    cursor.executemany(
+        """
+        INSERT INTO film_work(id, title, description, type, rating, created_at, updated_at) 
+        VALUES($1, $2, $3, $4, $5, $6, $7)
+        """,
+        [
+            (
+                str(uuid.uuid4()),
+                film[0],
+                film[1],
+                "movies",
+                film[2],
+                datetime.utcnow().replace(tzinfo=timezone.utc),
+                datetime.utcnow().replace(tzinfo=timezone.utc)
+            ) for film in films
+        ]
+    )
+
+
 if __name__ == "__main__":
     conn = sqlite3.connect("db.sqlite")
     cursor = conn.cursor()
 
     load_genres(cursor)
     load_person(cursor)
+    load_films(cursor)
 
     conn.commit()
     conn.close()
