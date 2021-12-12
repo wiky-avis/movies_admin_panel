6271a1349fc9e8767b86b40db6e333544a9fbdf3
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2021-12-12 20:33:48 +0300

    создание приложения, настройки

---
+++ movies_admin/config/settings/production.py
@@ -0,0 +1,14 @@
+from .base import *
+import os
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ['DB_ENGINE'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+    }
+}
