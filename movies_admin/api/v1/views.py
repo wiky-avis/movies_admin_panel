76d793720f0199d8ceb0a1fc9f99780c52e0f1f3
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2021-12-20 09:20:58 +0300

    views.py

--- movies_admin/api/v1/views.py
+++ movies_admin/api/v1/views.py
@@ -6,7 +6,6 @@ from api.v1.mixins import MoviesApiMixin
 
 class MoviesListApi(MoviesApiMixin, BaseListView):
     paginate_by = 50
-    ordering = "title"
 
     def get_context_data(self, *, object_list=None, **kwargs):
         paginator, page, queryset, is_paginated = self.paginate_queryset(
